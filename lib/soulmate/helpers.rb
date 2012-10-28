5302de1df39abb87dbc3593aef4b577d4880501f
Tanomsak Khacharoen <tanomsak.k@gmail.com>
2012-10-28 13:38:10 +0700

    support indexing non ascii character

--- lib/soulmate/helpers.rb
+++ lib/soulmate/helpers.rb
@@ -11,7 +11,8 @@ module Soulmate
     end
 
     def normalize(str)
-      str.downcase.gsub(/[^a-z0-9 ]/i, '').strip
+      # str.downcase.gsub(/[^a-z0-9 ]/i, '').strip
+      str.downcase.strip # keep thai char      
     end
 
   end
